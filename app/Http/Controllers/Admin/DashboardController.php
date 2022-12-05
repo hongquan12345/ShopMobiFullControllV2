f74eb1770f8a393f6af18ea1d2ff6a85cbb93514
NguyenHongQuan_FPT_Aptech <lactuong64@gmail.com>
2022-12-06 00:14:47 +0700

    update Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Brand;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $productTotal = Product::count();
+        $categoryTotal = Category::count();
+        $BrandTotal = Brand::count();
+
+        $accountTotal = User::count();
+        $userTotal = User::where('role_as','0')->count();
+        $AdminTotal = User::where('role_as','1')->count();
+
+        $todayDate = Carbon::now()->format('d-m-Y');
+        $MountDate = Carbon::now()->format('m');
+        $YeahDate = Carbon::now()->format('Y');
+
+        $orderTotal = Order::count();
+        $ordertoday = Order::whereDate('created_at',$todayDate)->count();
+        $orderMonth = Order::whereMonth('created_at',$MountDate)->count();
+        $orderYear = Order::whereYear('created_at',$YeahDate)->count();
+
+
+
+        return view('admin.dashboard',compact('orderTotal',
+
+        'ordertoday','orderMonth','orderYear',
+        'productTotal','BrandTotal','categoryTotal',
+    'accountTotal','userTotal','AdminTotal'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,32 +1,93 @@
 @extends('layouts.adminpage')
 @section('content')
-<div class="row"> 
+<div class="row">
     <div class="col-md-12 grid-margin">
-      <div class="d-flex justify-content-between flex-wrap">
-        <div class="d-flex align-items-end flex-wrap">
-          <div class="me-md-3 me-xl-5">
+        @if(session('message'))
+        <h6 class="alert alert-success">{{ session('message') }} </h6>
+        @endif
+        <div class="me-md-3 me-xl-5">
             <h2>Welcome back, {{Auth::user()->name}}</h2>
             <p class="mb-md-0">Your analytics dashboard template.</p>
-          </div>
-          <div class="d-flex">
-            <i class="mdi mdi-home text-muted hover-cursor"></i>
-            <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
-            <p class="text-primary mb-0 hover-cursor">Analytics</p>
-          </div>
         </div>
-        <div class="d-flex justify-content-between align-items-end flex-wrap">
-          <button type="button" class="btn btn-light bg-white btn-icon me-3 d-none d-md-block ">
-            <i class="mdi mdi-download text-muted"></i>
-          </button>
-          <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
-            <i class="mdi mdi-clock-outline text-muted"></i>
-          </button>
-          <button type="button" class="btn btn-light bg-white btn-icon me-3 mt-2 mt-xl-0">
-            <i class="mdi mdi-plus text-muted"></i>
-          </button>
-          <button class="btn btn-primary mt-2 mt-xl-0">Generate report</button>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-body bg-primary text-white mb-3 shadow bg-1 p-3 ">
+                    <label for="">Total Orders</label>
+                    <h1>{{ $orderTotal }}</h1>
+                    <a href="{{ url('adminpage/Orders') }}" class="text-white">View</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body bg-success text-white mb-3 shadow bg-1 p-3">
+                    <label for="">To Day Orders</label>
+                    <h1>{{ $ordertoday }}</h1>
+                    <a href="{{ url('adminpage/Orders') }}" class="text-white">View</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body bg-warning text-white mb-3 shadow bg-1 p-3">
+                    <label for="">To Mount Orders</label>
+                    <h1>{{ $orderMonth }}</h1>
+                    <a href="{{ url('adminpage/Orders') }}" class="text-white">View</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body bg-danger text-white mb-3 shadow bg-1 p-3">
+                    <label for="">To Year Orders</label>
+                    <h1>{{ $orderYear }}</h1>
+                    <a href="{{ url('adminpage/Orders') }}" class="text-white">View</a>
+                </div>
+            </div>
         </div>
-      </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-body bg-primary text-white mb-3 shadow bg-1 p-3">
+                    <label for="">Total Product</label>
+                    <h1>{{ $productTotal }}</h1>
+                    <a href="{{ url('adminpage/Products') }}" class="text-white">View</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body bg-success text-white mb-3 shadow bg-1 p-3">
+                    <label for="">Tota Brand</label>
+                    <h1>{{ $BrandTotal }}</h1>
+                    <a href="{{ url('adminpage/brands') }}" class="text-white">View</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body bg-warning text-white mb-3 shadow bg-1 p-3">
+                    <label for="">Total Category</label>
+                    <h1>{{ $categoryTotal }}</h1>
+                    <a href="{{ url('adminpage/Category') }}" class="text-white">View</a>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-body bg-primary text-white mb-3 shadow bg-1 p-3">
+                    <label for="">Total Acount</label>
+                    <h1>{{ $accountTotal }}</h1>
+                    <a href="{{ url('adminpage/Products') }}" class="text-white">View</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body bg-success text-white mb-3 shadow bg-1 p-3">
+                    <label for="">Tota User</label>
+                    <h1>{{ $userTotal }}</h1>
+                    <a href="{{ url('adminpage/brands') }}" class="text-white">View</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body bg-warning text-white mb-3 shadow bg-1 p-3">
+                    <label for="">Total Admin</label>
+                    <h1>{{ $AdminTotal }}</h1>
+                    <a href="{{ url('adminpage/Category') }}" class="text-white">View</a>
+                </div>
+            </div>
+        </div>
+        <hr>
     </div>
   </div>
 @endsection
